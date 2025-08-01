468eda7c5b9780091996cf7c33b5118e4229762c
F Cesar Farias <fcesarf80@gmail.com>
2025-08-01 16:27:25 +0100

    struct pontos

--- 02_cpp_class/structs.cpp
+++ 02_cpp_class/structs.cpp
@@ -1,4 +1,126 @@
-#include<iostream>              //Destrutor. exemplo com índice 
+#include<iostream>
+using namespace std;
+struct Ponto3D
+{
+    int X;
+    int Y;
+    int Z;
+
+}
+
+void DesenharPontos3D (int X, int Y, int Z)
+{
+    cout<< "\nX: " << x << "Y: " << y << "Z: " << z << "\n";
+}
+
+int main()
+{
+    system("pause");
+    return 0;
+}
+
+/*
+union abc{
+    int a;
+    char b;
+    //o tipo string não pode ser utilizado
+};
+
+@include<iostream>
+int main()
+{
+    abc elem;
+    elem.a=10;
+    cout<<"A: "<<elem.a <<endl;
+    elem.b='a';
+    cout<<"B: "<<elem.b <<endl;
+    cout<<"Tamanho ocupado pela union: "<<sizeof(union abc)<<endl;
+}
+
+ex2 item;
+int escolha, tipo;
+cout<<"Escolha o tipo de dado que deseja armazenar?"<<end;
+cout<<"1-Inteiro"<<endl;
+cout<<"2-Float"<<endl;
+cout<<"3-Decimal"<<endl;
+cout<<"opção?"<<endl;
+
+switch(escolha)
+
+case 2:
+cout<<"Decimal";
+cin>>item.f;
+tipo=2;
+break
+
+return 0;
+{
+atividade 3 - struct & Union
+union infoProduto
+{
+    int quantidade;
+    float preco;
+};
+
+struct Produto
+{
+    int ID;
+    string nome;
+    infoProduto info;
+    char tipoInfo;//q - quantidade e p / preco
+}
+Produto P cout<<"Cadastro do Produto:\n";
+//Leitura de dados
+cout<<"ID?"; cin>>p.ID;
+cib.ignore();
+
+cout<<"Nome do produto";
+getline(cin, p.nome)
+
+int escolha, tipo;
+cout<<"Escolha o tipo de informação  que deseja armazenar?"<<end;
+cout<<"1-Quantidade"<<endl;
+cout<<"2-Preço"<<endl;
+cout<<"opção?"<<endl;
+cin>>escolha;
+
+switch(escolha)
+
+case 1:
+cout<<"Quantidade?";
+cin>>p.infoProduto.quantidade;;
+p.tipoInfo=q;
+break
+case 2:
+cout<<"Preço?";
+cin>>p.infoProduto.preco;;
+p.tipoInfo=p;
+break
+default:
+cout<<"Opcao invalida";
+return 1;
+}
+
+//Exibir os dados
+cout<<"\n ----Detalhe do Produto---\n\n";
+cout<<"ID: "<<p.id<<endl;
+cout<<"Nome: "<<p.nome<<endl;
+
+if(p.topoInfo=='q')
+{
+    cout<<"Quantidade em stock: "<<p.Info.quantidade<<endl;
+else if (p.topoInfo=='p') {
+    cout<<"Preço produto: "<<p.Info.preco<<endl;
+}else{
+    cout<<"Tipo desconecido\n";
+}
+//enum ou enumerações
+
+return 0;
+}
+*/
+
+/*#include<iostream>              //Destrutor. exemplo com índice 
 #include<stdlib.h>              //PONTEIRO
 using namespace std;
 struct Data     //Como não foi especificado o modificador de
@@ -6,7 +128,7 @@ struct Data     //Como não foi especificado o modificador de
 {   
     static int contador;
     int id;
-    int Dia;  
+    int Dia;
     int Mes;  
     int Ano;      
     Data()
@@ -42,7 +164,7 @@ int main()
     cout<<endl;
     system("pause");
     return 0;
-}
+}*/
 
 /*
 #include<iostream>              //Destrutor. exemplo com índice 

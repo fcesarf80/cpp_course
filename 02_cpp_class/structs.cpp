aeef92024fa973666edd32c757d931d9031299d2
F Cesar Farias <fcesarf80@gmail.com>
2025-08-01 17:57:25 +0100

    livro

--- 02_cpp_class/structs.cpp
+++ 02_cpp_class/structs.cpp
@@ -1,24 +1,83 @@
-#include<iostream>
+//Crie um prog que receba os dados: ID, título, autor, editora e anos de um livro e coloquenestes dados na tela.
+#include <iostream>
+#include <string>
 using namespace std;
-struct Ponto3D
-{
-    int X;
-    int Y;
-    int Z;
 
-}
+/*Criada um struct, um modelo para criação de objetos deste tipo
+Importante destacar que até aqui no final */
 
-void DesenharPontos3D (int X, int Y, int Z)
+struct Livro
 {
-    cout<< "\nX: " << x << "Y: " << y << "Z: " << z << "\n";
-}
+	int ID;
+	std::string Titulo;
+	std::string Autor;
+	std::string Editora;
+};
+//aqui não existirá criação de nada na memória RAM, isso ou esta struct é apenas um modelo para criação de outras variáveis
+/*quando fora criada uma variavel do tipo desta struct, o compilador irá usar este molde, este modelo para arrumar e armazenar dados na memória RAM*/
 
 int main()
 {
-    system("pause");
-    return 0;
+	//Aqui foi criada uma variavel de nome livro(poderia ser outro nome)
+	//como livro é diferente de Livro pode ser feito, mas confunde...
+	/*logo aqui temos a criação na memória RAM de uma variável de nome livro
+	e que será criada a partir do modelo da struct Livro*/
+	//Em c++ podemos retirar a palavra struct ao criar uma variável do tipo struct
+
+	struct Livro livro;
+
+	//A partir de agora podemos acessar os campos desta variavel livro atraves da notação ponto(.)
+
+	cout << "\n***Cadastro de Livro***" << "\n";
+	cout << "Digite ID do Livro: ";
+	cin >> livro.ID;
+
+	cout << "\nDigite o Titulo do livro: ";
+	cin.ignore();
+	std::getline(cin, livro.Titulo); //obtenha linha = getline 
+
+	cout << "\nDigite o Autor do Livro: ";
+	std::getline(cin, livro.Autor);
+
+	cout << "\nDigite a Editor do Livro: ";
+	std::getline(cin, livro.Editora);
+
+	cout << "\nID: " << livro.ID << "\n";
+	cout << "Titulo: " << livro.Titulo << "\n";
+	std::cout << "Autor: " << livro.Autor << "\n";
+	std::cout << "Editora: " << livro.Editora << "\n";
+    cout<< endl;
+
+	system("PAUSE");
+	return 0;
 }
 
+/*
+#include <iostream>
+using namespace std;
+struct Ponto3D
+{
+	int X;
+	int Y;
+	int Z;
+	void DesenharPonto3D(Ponto3D Pontos)
+	{
+		cout << "\nX:" << Pontos.X << "\nY:" << Pontos.Y << "\nZ:" << Pontos.Z <<"\n" << endl;;
+	}
+};
+
+
+int main()
+{
+	Ponto3D PontoC;
+	PontoC.X = 3;
+	PontoC.Y = 4;
+	PontoC.Z = 5;
+	PontoC.DesenharPonto3D(PontoC);
+	system("PAUSE");
+	return 0;
+}
+*/
 /*
 union abc{
     int a;

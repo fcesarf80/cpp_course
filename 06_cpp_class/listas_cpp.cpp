f8088b6ac0ac36ac26a6d5bc0afa764b8683a29a
F Cesar Farias <fcesarf80@gmail.com>
2025-08-30 13:25:17 +0100

    zumba F

--- 06_cpp_class/listas_cpp.cpp
+++ 06_cpp_class/listas_cpp.cpp
@@ -1,3 +1,59 @@
+#include<list>
+#include<iostream>
+using namespace std;
+struct Person{
+    string name;
+    int age; };
+void LerPerson(list<Person> &person, int quant){
+    for(int i=0; i<quant; i++){
+        Person p;
+        cout<<"Digite o nome da pessoa "<<i+1<<": ";
+        getline(cin, p.name);
+        cout<<"Digite a idade da pessoa " <<i+1<<": ";
+        cin>>p.age;
+        cin.ignore(); //clear buffer
+        person.push_back(p);
+    }
+}
+void imprimirPerson(list<Person> &person){
+    cout<< "\nLista de Pessoas:\n\n";
+    list<Person>::iterator it;
+    for(it=person.begin(); it!=person.end(); it++){
+        cout<<"Nome: "<<it->name<<", idade: "<<it->age<<endl;
+    }
+ 
+}
+int main(){
+    list<Person> person;
+    int tam=3;
+    LerPerson(person, tam);
+    imprimirPerson(person);
+}
+/*#include<list>
+#include<iostream>
+using namespace std;
+int main(){ list <int> num;
+    list <int>::iterator it; int valor;
+//inserir numeros pelo utilizador
+    cout<<"\nInsira 10 digitos inteiros:\n\n";
+    for(int i=0; i<10; i++){ cout<<"Valor? ";
+        cin>>valor;
+        num.push_front(valor);   }
+    //imprimir lista original
+    cout<<"\nLista original:\n";
+    for(it=num.begin(); it!=num.end();it++){
+        cout<<*it<<" ";
+        it=num.erase(it);    }
+    //remover numeros impares
+    for(it=num.begin(); it!=num.end();it++){
+        if(*it%2!=0){ num.erase(it);
+         } else{ it++;}    }
+    //imprimir listas sem números
+    cout<<"\nLista sem números impares:\n\n";
+    for(it=num.begin(); it!=num.end();it++){
+        cout<<*it<< " "; }
+        return 0;
+}*/
 /*#include<iostream>
 #include<list>
 using namespace std;
